fade98fb814245dc8ad3dcccaac4d89942f8123a
king <wangmaming@126.com>
2019-06-12 16:40:00 +0800

    Add files via upload

---
+++ bert_中文分类/train_bert__lstm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export BERT_BASE_DIR=../chinese_L-12_H-768_A-12
+
+
+python run_bert_lstm.py \
+  --task_name=ming \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=./data \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/data/antispam/kingming/bert-master/output1/
